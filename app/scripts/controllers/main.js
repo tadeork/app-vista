943e38eba008d11cbaf0efd2c174936760e54b8b
Tadeo <tadeoriverosk@gmail.com>
2014-05-28 00:29:06 -0300

    Vista con controladores básicos en AngularJS.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,20 +1,22 @@
 'use strict';
 
-var appTwitt = angular.module('appVistaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-});
+var appTwitt = angular.module('appVistaApp'); //módulo
 
-appTwitt.controller('SearchTwittsController', 
-    function($scope, $resource){
-        $scope.twits = [
-            {'id':1,'contendio':'Carne a buen precio', 'precio':30.50},
-            {'id':2,'contendio':'Carne a buen precio', 'precio':30.50},
-            {'id':3,'contendio':'Carne a buen precio', 'precio':30.50},
-            {'id':4,'contendio':'Carne a buen precio', 'precio':30.50}
-        ];
-});
+appVistaApp.controller('MainCtrl', //controlador del módulo
+        function($scope) {
+            $scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+            $scope.querySearch = null;
+            $scope.walkLess = null;
+            $scope.spendLess = null;
+
+            $scope.search = function() {
+                console.log($scope.querySearch);
+                console.log($scope.walkLess);
+                console.log($scope.spendLess);
+            };
+        });
+console.log('soy el controlador del módulo');
